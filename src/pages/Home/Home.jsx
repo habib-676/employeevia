f2a3eeff23c0f8a2f27c544664bd0f33ce0c41ef
habib-676 <rahman.habibur2421k@gmail.com>
2025-07-13 23:40:30 +0600

    add meet-our-team section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Team from "./our-team/Team";
 import Services from "./Services/Services";
 import Slider from "./Slider/Slider";
 import Testimonials from "./Testimonial/Testimonials";
 const servicesPromise = fetch("services.json").then((res) => res.json());
 const testimonialPromise = fetch("testimonials.json").then((res) => res.json());
+const teamPromise = fetch("team.json").then((res) => res.json());
 const Home = () => {
   return (
     <div>
@@ -10,6 +12,9 @@ const Home = () => {
       <Services servicesPromise={servicesPromise}></Services>
       <div>
         <Testimonials testimonialPromise={testimonialPromise}></Testimonials>
+      </div>
+      <div>
+        <Team teamPromise={teamPromise}></Team>
       </div>
     </div>
   );
---
+++ src/pages/Home/our-team/Team.jsx
@@ -0,0 +1,26 @@
+import React, { use } from "react";
+import Container from "../../../components/Shared/Container/Container";
+import Person from "./Person";
+
+const Team = ({ teamPromise }) => {
+  const teamData = use(teamPromise);
+  return (
+    <Container>
+      <div className="mt-24">
+        <h2 className="text-center text-4xl font-bold">Meet Our Team</h2>
+        <p className="text-center font-medium text-secondary-content">
+          Real people behind the scenes
+        </p>
+        <div className="card-body card shadow-2xl bg-base-200 border border-primary mt-10 min-h-[60vh]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
+            {teamData.map((person) => (
+              <Person key={person.id} person={person}></Person>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Team;
