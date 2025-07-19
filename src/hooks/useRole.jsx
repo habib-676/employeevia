eed1661730ffacc63ace1c1728c9dd583e697c07
habib-676 <rahman.habibur2421k@gmail.com>
2025-07-19 22:52:17 +0600

    add useRole hook

---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,22 @@
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+import { useQuery } from "@tanstack/react-query";
+
+const useRole = () => {
+  const { user, loading } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: role, isLoading: isRoleLoading } = useQuery({
+    queryKey: ["role", user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users/role/${user?.email}`);
+      return data;
+    },
+  });
+
+  console.log(role);
+  return [role, isRoleLoading];
+};
+
+export default useRole;
